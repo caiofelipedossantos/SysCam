1be73b904fad6a9a76c53bd77b1fed3906b37166
Caio Felipe <caiofelipe.up@gmail.com>
2018-04-12 00:52:48 -0300

    Atualização de Estrutura

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    
-</body>
+   <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>SysCam</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Bootstrap 4.1 -->
+      <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+      </script>
+      <!-- Main CSS -->
+      <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css" />
+   </head>
+   <body>
+      
+      <!-- JQuery 3.3.1 -->
+      <script src="plugins/jQuery/jquery-3.3.1.min.js"></script>
+      <!-- Bootstrap 4.1 -->
+      <script src="bootstrap/js/bootstrap.min.js"></script>
+      <!-- Main JS -->
+      <script src="assets/js/main.js"></script>
+   </body>
 </html>
